854f0f6b9b04d1d0111ceac14ed4efa0738c7620
CDalianis <christos_dl@hotmail.com>
2026-06-28 22:59:44 +0300

    feat add: Digital Detox Application Test init commit

---
+++ digital-detox-project/src/test/java/com/digitaldetox/DigitalDetoxApplicationTests.java
@@ -0,0 +1,14 @@
+package com.digitaldetox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class DigitalDetoxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
